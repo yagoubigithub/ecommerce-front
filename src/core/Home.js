88a49e052d33abc4e47e03fc1121af340f81c732
yagoubigithub <yagoubi.aek.2@gmail.com>
2021-05-11 09:39:25 +0100

    show product

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,12 +1,77 @@
 
-import React from 'react'
+import React , {useState, useEffect} from 'react'
 
 import Layout from './Layout'
+import Card from './Card'
+import {getProducts} from './apiCore'
 
-const Home = ()=>(<Layout title="Home Page" description="Node React e commerce website">
+
+const Home = ()=>{
+
+   const [productsBySell,setProductsBySell] =  useState([])
+   const [productsByArrival,setProductsByArrival] =  useState([])
+   const [error,setError] =  useState(false)
+
+
+   const loadProductsBySell = () =>{
+
+    getProducts("sold").then(
+        data =>{
+            if(data.error){
+                setError(data.error)
+            }else{
+                setProductsBySell(data)
+            }
+        }
+    )
+   }
+
+
+   
+   const loadProductsByArrival= () =>{
+
+    getProducts("createdAt").then(
+        data =>{
+            if(data.error){
+                setError(data.error)
+            }else{
+                setProductsByArrival(data)
+            }
+        }
+    )
+   }
+
+   useEffect(() => {
+     loadProductsByArrival()
+     loadProductsBySell()
+   }, [])
+    return (<Layout title="Home Page" description="Node React e commerce website" className="container-fluid">
+
+
+<h2 className="mb-2">New Arrivals</h2>
+<div className="row">
+
+
+{productsByArrival.map((product,i)=>(
+    <Card key={i} product={product} />
+))}
+
+</div>
+
+<h2 className="mb-2">Best Sellers</h2>
+<div className="row">
+
+{productsBySell.map((product,i)=>(
+    <Card key={i} product={product} />
+))}
+
+</div>
 
 
 
-</Layout>);
 
+
+    </Layout>);
+    
+}
 export default Home;
---
+++ src/core/apiCore.js
@@ -0,0 +1,14 @@
+import { API } from "../Config";
+
+
+
+export const getProducts = ( sortBy ) =>{
+    return fetch(`${API}/products?sortBy=${sortBy}&order=desc&limit=6`,
+    {
+      method : "GET" 
+     })
+     .then(responce=>{
+       return responce.json()
+     })
+     .catch(err=>console.log(err))
+  }
