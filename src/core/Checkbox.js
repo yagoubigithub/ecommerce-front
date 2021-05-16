c20d7c729785855a6a468aa5baebc4751cfc9fc4
yagoubigithub <yagoubi.aek.2@gmail.com>
2021-05-16 11:38:02 +0100

    checkbox logic

--- src/core/Checkbox.js
+++ src/core/Checkbox.js
@@ -3,11 +3,33 @@ import React, { useState, useEffect } from 'react';
 
 
 const Checkbox = ({categories}) => {
+
+    const [checked, setChecked] = useState([]);
+
+    const handleToggle = c => () =>{
+
+        // return the first index of -1
+        const currentCategoryId = checked.indexOf(c) 
+
+        const newCheckedCategoryId = [...checked];
+
+        //if currently checked was not alredy in checked state> push
+        //else pull/take off
+        if(currentCategoryId == -1){
+            newCheckedCategoryId.push(c)
+        }else{
+            newCheckedCategoryId.splice(currentCategoryId , 1)
+        }
+        console.log(newCheckedCategoryId)
+        setChecked(newCheckedCategoryId)
+
+
+    }
     return categories.map((c,i)=>{
 
         return (
             <li key={i} className="list-unstyled">
-            <input type="checkbox" className="form-check-input" />
+            <input onChange={handleToggle(c._id)} value={checked.indexOf(c._id === -1)} type="checkbox" className="form-check-input" />
             <label  className="form-check-label" >{c.name}</label>
            
 
