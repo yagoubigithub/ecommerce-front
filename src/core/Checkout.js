4a94114d8f3944997b8bd1955d3afb373524d346
yagoubigithub <yagoubi.aek.2@gmail.com>
2021-06-01 01:36:37 +0100

    show braintree payment layout

--- src/core/Checkout.js
+++ src/core/Checkout.js
@@ -2,12 +2,38 @@ import React, { useState, useEffect } from "react";
 
 import Layout from "./Layout";
 import Card from "./Card";
-import { getProducts } from "./apiCore";
+
+import DropIn from "braintree-web-drop-in-react";
+import { getBraintreeClientToken } from "./apiCore";
 
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
 
 const Checkout = ({ products }) => {
+  const [data, setData] = useState({
+    success: false,
+    clientToken: null,
+    error: "",
+    instance: {},
+    address: "",
+  });
+
+  const userId = isAuthenticated() && isAuthenticated().user._id;
+  const token = isAuthenticated() && isAuthenticated().token;
+
+  const getToken = (userId, token) => {
+    getBraintreeClientToken(userId, token).then((data) => {
+      if (data.error) {
+        setData({ ...data, error: data.error });
+      } else {
+        setData({ ...data, clientToken: data.clientToken });
+      }
+    });
+  };
+  useEffect(() => {
+    getToken(userId, token);
+  }, []);
+
   const getTotal = () => {
     return products.reduce((currentValue, nextValue) => {
       return currentValue + nextValue.count * nextValue.price;
@@ -17,13 +43,32 @@ const Checkout = ({ products }) => {
   const showCheckout = () => {
     {
       return isAuthenticated() ? (
-        <button className="btn btn-success">Checkout </button>
+        <div>{showDropIn()} </div>
       ) : (
         <Link to="/signin">
           <button className="btn btn-primary">Sign in to Checkout </button>
         </Link>
       );
     }
+  };
+
+  const showDropIn = () => {
+    return (
+      <div>
+        {data.clientToken !== null && products.length > 0 ? (
+          <div>
+            <DropIn
+              options={{
+                authorization: data.clientToken,
+              }}
+              onInstance={(instance) => (data.instance = instance)}
+            />
+
+            <button className="btn btn-success">Checkout</button>
+          </div>
+        ) : <div>hesedff</div>}
+      </div>
+    );
   };
   return (
     <div>
--- src/core/apiCore.js
+++ src/core/apiCore.js
@@ -87,3 +87,19 @@ export const listRelated = ( productId) =>{
    .catch(err=>console.log(err))
 }
 
+export const getBraintreeClientToken = ( userId, token) =>{
+  return fetch(`${API}/braintree/getToken/${userId}`,
+  {
+    method : "GET" ,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization : `Bearer ${token}`
+    },
+   })
+   .then(responce=>{
+     return responce.json()
+   })
+   .catch(err=>console.log(err))
+}
+
