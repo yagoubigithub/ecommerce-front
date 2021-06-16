bc7b5925fa9edf824dda9fb1c851593367376ed9
yagoubigithub <yagoubi.aek.2@gmail.com>
2021-06-16 07:12:16 +0100

    user history

--- src/user/apiUser.js
+++ src/user/apiUser.js
@@ -48,3 +48,19 @@ export const update = (userId, token, user) => {
   }
   
   
+
+  
+export const getPurchaseHistory = (userId, token) => {
+    return fetch(`${API}/orders/by/user/${userId}`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((responce) => {
+        return responce.json();
+      })
+      .catch((err) => console.log(err));
+  };
--- src/user/userDashboard.js
+++ src/user/userDashboard.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React  ,{useState ,useEffect}from "react";
 import { Link } from "react-router-dom";
 
 import Layout from "../core/Layout";
+import moment from "moment"
 
 import { isAuthenticated } from "../auth";
+import {getPurchaseHistory} from './apiUser'
 
 const Dashboard = () => {
+
+  const [history, setHistory] = useState([])
+
   const {
-    user: { _id, name, email, role },
+    user: { _id, name, email, role }, token
   } = isAuthenticated();
+
+  const init  = (userId, token) =>{
+    getPurchaseHistory(userId, token)
+    .then(data=>{
+      if(data.error){
+        console.log(data.error)
+      }else{
+        setHistory(data)
+      }
+    })
+  }
+  useEffect(() => {
+   init(_id, token)
+  }, [])
 
   const userLinks = () => {
     return (
@@ -46,16 +65,37 @@ const Dashboard = () => {
     );
   };
 
-  const pursheseHistory = () => {
-    return (
+  
+const purchaseHistory = history => {
+  return (
       <div className="card mb-5">
-        <h3 className="card-header">Project history</h3>
-        <ul className="list-group">
-          <li className="list-group-item">history</li>
-        </ul>
+          <h3 className="card-header">Purchase history</h3>
+          <ul className="list-group">
+              <li className="list-group-item">
+                  {history.map((h, i) => {
+                      return (
+                          <div key={i}>
+                              <hr />
+                              {h.products.map((p, i) => {
+                                  return (
+                                      <div key={i}>
+                                          <h6>Product name: {p.name}</h6>
+                                          <h6>Product price: ${p.price}</h6>
+                                          <h6>
+                                              Purchased date:{" "}
+                                              {moment(p.createdAt).fromNow()}
+                                          </h6>
+                                      </div>
+                                  );
+                              })}
+                          </div>
+                      );
+                  })}
+              </li>
+          </ul>
       </div>
-    );
-  };
+  );
+};
   return (
     <Layout
       title="Dashboard"
@@ -66,7 +106,7 @@ const Dashboard = () => {
         <div className="col-3">{userLinks()}</div>
         <div className="col-9">
           {userInfo()}
-          {pursheseHistory()}
+          {purchaseHistory(history)}
         </div>
       </div>
     </Layout>
